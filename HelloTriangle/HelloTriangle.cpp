4bbbbb0553d6b6e37a429b32e38593e2ddc656e8
陈国栋 <sinojelly@msn.cn>
2017-05-07 12:44:44 +0800

    use index buffer to draw rectangle.

--- HelloTriangle/HelloTriangle.cpp
+++ HelloTriangle/HelloTriangle.cpp
@@ -50,9 +50,18 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-	{ { 0.0f, -0.5f },{ 1.0f, 1.0f, 1.0f } },
-	{ { 0.5f, 0.5f },{ 0.0f, 1.0f, 0.0f } },
-	{ { -0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } }
+	//{ { 0.0f, -0.5f },{ 1.0f, 1.0f, 1.0f } },
+	//{ { 0.5f, 0.5f },{ 0.0f, 1.0f, 0.0f } },
+	//{ { -0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } }
+
+	{ {-0.5f, -0.5f},{ 1.0f, 0.0f, 0.0f }},
+	{ { 0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f } },
+	{ { 0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } },
+	{ { -0.5f, 0.5f },{ 1.0f, 1.0f, 1.0f } }
+};
+
+const std::vector<uint16_t> indices = {
+	0, 1, 2, 2, 3, 0
 };
 
 template <typename T>
@@ -155,6 +164,7 @@ private:
 		createFramebuffers();
 		createCommandPool();
 		createVertexBuffer();
+		createIndexBuffer();
 		createCommandBuffers();
 		createSemaphores();
 	}
@@ -269,6 +279,8 @@ private:
 	VDeleter<VkSemaphore> renderFinishedSemaphore{ device, vkDestroySemaphore };
 	VDeleter<VkBuffer> vertexBuffer{ device, vkDestroyBuffer };
 	VDeleter<VkDeviceMemory> vertexBufferMemory{ device, vkFreeMemory };
+	VDeleter<VkBuffer> indexBuffer{ device, vkDestroyBuffer };
+	VDeleter<VkDeviceMemory> indexBufferMemory{ device, vkFreeMemory };
 
 
 	bool checkValidationLayerSupport() {
@@ -1029,8 +1041,10 @@ private:
 			VkBuffer vertexBuffers[] = { vertexBuffer };
 			VkDeviceSize offsets[] = { 0 };
 			vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
+			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
 
-			vkCmdDraw(commandBuffers[i], vertices.size(), 1, 0, 0);
+			//vkCmdDraw(commandBuffers[i], vertices.size(), 1, 0, 0);
+			vkCmdDrawIndexed(commandBuffers[i], indices.size(), 1, 0, 0, 0);
 
 			vkCmdEndRenderPass(commandBuffers[i]);
 			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
@@ -1227,6 +1241,23 @@ private:
 		}
 
 		vkBindBufferMemory(device, buffer, bufferMemory, 0);
+	}
+
+	void createIndexBuffer() {
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+		VDeleter<VkBuffer> stagingBuffer{ device, vkDestroyBuffer };
+		VDeleter<VkDeviceMemory> stagingBufferMemory{ device, vkFreeMemory };
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+		copyBuffer(stagingBuffer, indexBuffer, bufferSize);
 	}
 
 };
